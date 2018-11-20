371326e1052b03b31f7a3b081dc6365b28c50773
Charlie <carlos.coque.chorlango>
2018-11-20 11:54:12 +0100

    EN ESTE COMIT ARREGLO EL FRAGMEMT DE MANAGE Y EL DE SLIDE SHOW

--- app/src/main/java/com/example/alumno/a10navigationyfragments/Fragments/SlideShowFragment.java
+++ app/src/main/java/com/example/alumno/a10navigationyfragments/Fragments/SlideShowFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.alumno.a10navigationyfragments.R;
 
@@ -12,12 +13,15 @@ public class SlideShowFragment extends Fragment {
 
     public SlideShowFragment() {
         // Required empty public constructor
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_slide_show, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_slide_show, container, false);
+        Toast.makeText(getActivity(), "Hola ", Toast.LENGTH_LONG).show();
+        return rootView;
     }
 }
